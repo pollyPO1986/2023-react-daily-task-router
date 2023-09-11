393fa0999fe38ff3430bf21093dd4e8ad465dfb2
PollyPO Lee <polly0215yu@gmail.com>
2023-09-11 17:31:59 +0800

    Day 18: React Router 動態路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import './App.css';
-import { HashRouter, NavLink, Routes, Route } from 'react-router-dom';
+import { HashRouter, NavLink, Routes, Route, useParams, Outlet } from 'react-router-dom';
 import Register from './pages/register.jsx';
 import Login from './pages/login.jsx';
 import Todo from './pages/todo.jsx';
+
+const Post = () => {
+  return (
+    <div>
+      <h3>Post 頁面</h3>
+      <Outlet />
+    </div>
+  );
+}
+
+const PostId = () => {
+  let parameters = useParams();
+
+  return (
+    <p>Post: {parameters.postId}</p>
+  );
+}
 
 function App () {
   return (
@@ -21,12 +38,19 @@ function App () {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
+          <Route path="/" element={<h1>首頁</h1>} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/todo" element={<Todo />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />} />
+          </Route>
         </Routes>
         {/* 練習區 */}
       </HashRouter>
